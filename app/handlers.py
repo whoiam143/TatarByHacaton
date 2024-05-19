ff21fdbe620c403f0ec428bf36ef5d5c0961550c
whoiam143 <emil2008sultan@icloud.com>
2024-05-19 01:04:31 +0000

    pre-relise

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,10 @@
+import asyncio
+
 from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.filters import CommandStart, Command
 from TatarByHacaton.translator import *
 
-from time import sleep
 
 from TatarByHacaton.data.db import *
 
@@ -17,10 +18,11 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer("""Привет🖐️, я Татарин, и помогу тебе узнать больше о Республике Татарстан и её культуре!
+    await message.answer("""Привет🖐️, я татарин Айдар, и помогу тебе узнать больше о Республике Татарстан и её культуре!
 -----------------------------------------
-Hi🖐️, I am a Tatarin, and I will help you know more about the Republic of Tatarstan and its culture!""")
+Hi🖐️, I am a Tatarin Aidar, and I will help you know more about the Republic of Tatarstan and its culture!""")
     await message.answer("Выбери язык / Choose language:", reply_markup=kb.start_kb)
+
 
 @router.callback_query(F.data == "ru")
 async def select_russian(callback: CallbackQuery):
@@ -45,6 +47,7 @@ async def back1(message: Message):
 async def back2(message: Message):
     await message.answer(get_text(8, kb.get_user_lang(message.from_user.id)),reply_markup=kb.eat_veiw(message.from_user.id))
 
+
 @router.message(F.text.in_(["Назад", "Back"]))
 async def back2(message: Message):
     await message.answer(get_text(8, kb.get_user_lang(message.from_user.id)),reply_markup=kb.eat_veiw(message.from_user.id))
@@ -53,13 +56,86 @@ async def back2(message: Message):
 ####################### GAME #######################
 
 @router.message(F.text.in_(["Начать путешествие🧭", "Start the trip🧭"]))
-async def lvl1(message: Message):
-    await message.answer("Какое-то вступление, фото")
+async def start(message: Message):
+    photo = FSInputFile("pictures/shit.jpg")
+    await message.answer_photo(photo, get_text(21, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(10)
+    photo2 = FSInputFile("pictures/karta1.jpg")
+    await message.answer_photo(photo2, get_text(22, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    photo3 = FSInputFile("pictures/bolgar_mech.jpg")
+    await message.answer_photo(photo3, get_text(23, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(13)
+    await message.answer(get_text(24, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest1(message.from_user.id))
 
+
+@router.message(F.text.in_(["Брутальный треугольник💪", "The Brutal Triangle💪", "Лапша быстрого приготовления🍜", "Instant noodles🍜"]))
+async def check1(message: Message):
+    await message.reply(get_text(30, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest1(message.from_user.id, ))
+
+
+@router.message(F.text.in_(["Три угла📐", "Three corners📐"]))
+async def lvl2_3(message: Message):
+    await message.reply(get_text(31, kb.get_user_lang(message.from_user.id)))
+    photo = FSInputFile("pictures/treygl1.jpg")
+    await message.answer_photo(photo, get_text(32, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    karta2 = FSInputFile("pictures/karta2.jpg")
+    await message.answer_photo(karta2, get_text(34, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    vid = FSInputFile("pictures/inno.mp4")
+    await message.answer_video(vid, caption=get_text(33, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(10)
+    photo2 = FSInputFile("pictures/treygl4.jpg")
+    await message.answer_photo(photo2, get_text(32, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    karta2 = FSInputFile("pictures/karta3.jpg")
+    await message.answer_photo(karta2, get_text(35, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    photo3 = FSInputFile("pictures/sviyajsk.jpg")
+    await message.answer_photo(photo3, get_text(36, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(10)
+    await message.answer(get_text(37, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest2(message.from_user.id))
+
+
+@router.message(F.text.in_(["C овощами🍅", "With vegetables 🍅", "C фруктами🍏", "With fruits🍏"]))
+async def check1(message: Message):
+    await message.reply(get_text(30, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest2(message.from_user.id, ))
+
+
+@router.message(F.text.in_(["C мясом и картошкой🍖🥔", "With meat and potatoes🍖🥔"]))
+async def lvl3_4(message: Message):
+    await message.reply(get_text(31, kb.get_user_lang(message.from_user.id)))
+    photo = FSInputFile("pictures/treygl3.jpg")
+    await message.answer_photo(photo, get_text(32, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    karta4 = FSInputFile("pictures/karta4.jpg")
+    await message.answer_photo(karta4, get_text(41, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    photo3 = FSInputFile("pictures/kazan.jpg")
+    await message.answer_photo(photo3, get_text(42, kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(10)
+    await message.answer("https://www.youtube.com/watch?v=rEqRoRZ96V4&ab_channel=tatarcha")
+    await asyncio.sleep(10)
+    await message.answer(get_text(46, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest3(message.from_user.id))
+
+
+@router.message(F.text.in_(["Богатая купчиха💰", "A rich merchant's wife💰", "Крестьянка👩", "Peasant woman 👩"]))
+async def check1(message: Message):
+    await message.reply(get_text(30, kb.get_user_lang(message.from_user.id)), reply_markup=kb.kvest3(message.from_user.id, ))
+
+
+@router.message(F.text.in_(["Правительница Казани👸", "The ruler of Kazan 👸"]))
+async def lvl3_4(message: Message):
+    await message.reply(get_text(31, kb.get_user_lang(message.from_user.id)))
+    photo = FSInputFile("pictures/treygl2.jpg")
+    await message.answer_photo(photo, get_text(47 , kb.get_user_lang(message.from_user.id)))
+    await asyncio.sleep(3)
+    gif = FSInputFile("pictures/ech.gif")
+    await message.answer_animation(gif, caption=get_text(48, kb.get_user_lang(message.from_user.id)), reply_markup=kb.menu_keyboard(message.from_user.id))
 
 
 ####################### TRAVEL GUIDE #######################Travel Guide 🗺
-
 @router.message(F.text.in_(["Travel Guide 🗺", "Путеводитель🗺"]))
 async def eating_place(message: Message):
     await message.answer(get_text(3, kb.get_user_lang(message.from_user.id)), reply_markup=kb.eat_veiw(message.from_user.id))
@@ -68,6 +144,7 @@ async def eating_place(message: Message):
 @router.message(F.text.in_(["Попробовать местную кухню🍴", "Try the cuisine🍴"]))
 async def eating(message: Message):
     await message.answer(get_text(7, kb.get_user_lang(message.from_user.id)), reply_markup=kb.eat(message.from_user.id))
+
 
 @router.message(F.text.in_(["Развлечься🤟", "Have fun🤟"]))
 async def fun(message: Message):
@@ -79,10 +156,12 @@ async def tubetey(message: Message):
     photo = FSInputFile("pictures/tubetey.jpg")
     await message.answer_photo(photo, get_text(9, kb.get_user_lang(message.from_user.id)), reply_markup=kb.eat(message.from_user.id))
 
+
 @router.message(F.text == "2. Гусь")
 async def tubetey(message: Message):
     photo = FSInputFile("pictures/gus.jpg")
     await message.answer_photo(photo, get_text(10, kb.get_user_lang(message.from_user.id)), reply_markup=kb.eat(message.from_user.id))
+
 
 @router.message(F.text == "3. Азу")
 async def tubetey(message: Message):
@@ -95,10 +174,12 @@ async def tubetey(message: Message):
     photo = FSInputFile("pictures/skypark.jpg")
     await message.answer_photo(photo, get_text(14, kb.get_user_lang(message.from_user.id)), reply_markup=kb.fun(message.from_user.id))
 
+
 @router.message(F.text == "2. Форсаж")
 async def tubetey(message: Message):
     photo = FSInputFile("pictures/forsaj.jpg")
     await message.answer_photo(photo, get_text(15, kb.get_user_lang(message.from_user.id)), reply_markup=kb.fun(message.from_user.id))
+
 
 @router.message(F.text == "3. LazerLand")
 async def tubetey(message: Message):
@@ -106,12 +187,12 @@ async def tubetey(message: Message):
     await message.answer_photo(photo, get_text(16, kb.get_user_lang(message.from_user.id)), reply_markup=kb.fun(message.from_user.id))
 
 
-
 ####################### TRANSLATER #######################
 @router.business_message()
 async def check_business_message(message: Message):
     if message.from_user.id != message.chat.id:
         await message.answer(translate_to_tat(message.text))
+
 
 @router.message(Command('translate'))
 async def check_message(message: Message):
--- app/keyboards.py
+++ app/keyboards.py
@@ -11,39 +11,66 @@ start_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="RU�
 def menu_keyboard(user_id):
     menu_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=str(get_text(1, get_user_lang(user_id))))],
                                             [KeyboardButton(text=str(get_text(2, get_user_lang(user_id))))]],
-                                  resize_keyboard=True, input_field_placeholder="Выбери опцию:")
+                                  resize_keyboard=True, one_time_keyboard=True,
+                                  input_field_placeholder=str(get_text(28, get_user_lang(user_id))))
     return menu_kb
+
 
 ########### GAME #############
 def kvest1(user_id):
-    kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="1 отве", callback_data="ru")],
-                                                 [InlineKeyboardButton(text="EN🇺🇸", callback_data="en")]],
-                                resize_keyboard=True)
+    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=str(get_text(25, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(26, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(27, get_user_lang(user_id))))]],
+                                  resize_keyboard=True, one_time_keyboard=True,
+                             input_field_placeholder=str(get_text(29, get_user_lang(user_id))))
+    return kb
+
+
+def kvest2(user_id):
+    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=str(get_text(38, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(39, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(40, get_user_lang(user_id))))]],
+                                  resize_keyboard=True, one_time_keyboard=True,
+                             input_field_placeholder=str(get_text(29, get_user_lang(user_id))))
+    return kb
+
+
+def kvest3(user_id):
+    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=str(get_text(43, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(44, get_user_lang(user_id))))],
+                                       [KeyboardButton(text=str(get_text(45, get_user_lang(user_id))))]],
+                                  resize_keyboard=True, one_time_keyboard=True,
+                             input_field_placeholder=str(get_text(29, get_user_lang(user_id))))
+    return kb
 
 ########## TRAVEL GUIDE ##########
+
 
 def eat_veiw(user_id):
     kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=str(get_text(4, get_user_lang(user_id))))],
                                        [KeyboardButton(text=str(get_text(5, get_user_lang(user_id))))],
                                        [KeyboardButton(text=str(get_text(6, get_user_lang(user_id))))]],
-                                  resize_keyboard=True, input_field_placeholder="Выбери опцию:")
+                                  resize_keyboard=True, input_field_placeholder=str(get_text(28, get_user_lang(user_id))))
     return kb
+
 
 def eat(user_id):
     kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="1. Тюбетей")],
                                        [KeyboardButton(text="2. Гусь")],
                                        [KeyboardButton(text="3. Азу")],
                                        [KeyboardButton(text=str(get_text(8, get_user_lang(user_id))))]],
-                                  resize_keyboard=True, input_field_placeholder="Выбери опцию:")
+                                  resize_keyboard=True, input_field_placeholder=str(get_text(28, get_user_lang(user_id))))
     return kb
+
 
 def fun(user_id):
     kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="1. Skypark")],
                                        [KeyboardButton(text="2. Форсаж")],
                                        [KeyboardButton(text="3. LazerLand")],
                                        [KeyboardButton(text=str(get_text(13, get_user_lang(user_id))))]],
-                                  resize_keyboard=True, input_field_placeholder="Выбери опцию:")
+                                  resize_keyboard=True, input_field_placeholder=str(get_text(28, get_user_lang(user_id))))
     return kb
+
 
 def user_lang(user_id, lang):
     with sql.connect('data/dictionary.sqlite3') as db:
